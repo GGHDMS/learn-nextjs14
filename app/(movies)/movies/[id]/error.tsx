93861659f3890af8a12c883eb0f9aad96fe9c060
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-05-04 19:50:29 +0900

    Error Handling

---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ErrorOMG() {
+  return (
+    <div>
+      <h1>OMG! An error occurred!</h1>
+    </div>
+  );
+}
--- component/movie-videos.tsx
+++ component/movie-videos.tsx
@@ -3,8 +3,9 @@ import { API_URL } from "../app/constants";
 async function getVideos(id: String) {
   console.log(`Fetching videos: ${Date.now()}`);
   await new Promise((resolve) => setTimeout(resolve, 3000));
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
+  throw new Error("Error in getVideos");
+  // const response = await fetch(`${API_URL}/${id}/videos`);
+  // return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: String }) {
